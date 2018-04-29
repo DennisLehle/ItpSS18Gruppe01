6aa3182876cd39d5d347c944f6d30f546a3ad862
DennisLehle <dl043@hdm-stuttgart.de>
2018-04-29 22:14:31 +0200

    DB Connection Klasse eingerichtet

---
+++ src/de/hdm/itprojektss18/team01/sontact/server/db/DBConnection.java
@@ -0,0 +1,73 @@
+package de.hdm.itprojektss18.team01.sontact.server.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import com.google.appengine.api.utils.SystemProperty;
+
+/**
+ * Klasse für den Aufbau zur Datenbank mittels JDBC.
+ * 
+ * @author Dennis Lehle
+ */
+public class DBConnection{
+
+	private static Connection con = null;
+	private static String googleUrl = "jdbc:google:mysql://NameDeployAppEngine:europe-west:LinkDeploy?user=root&password=1234";
+	private static String localUrl = "jdbc:mysql://localhost:3306/DbName?user=root&password=";
+//	private static String localUrl = "jdbc:mysql://localhost:3308/itproj?user=root&password=";
+
+     /**
+      * Diese Methode gibt die aufgebaute DB-Verbindung zurück
+      * @return con
+      */
+    public static Connection connection(){
+
+    		/**
+    		 *  Wenn es bisher keine Connection zur DB gab, ...
+    		 */
+        if (con == null) {
+            String url = null;
+            try {
+            
+            	if (SystemProperty.environment.value() == SystemProperty.Environment.Value.Production) {       
+            		/**
+                	 * Load the class that provides the new "jdbc:google:mysql://" prefix.
+                	 */
+                	Class.forName("com.mysql.jdbc.GoogleDriver");
+                    url = googleUrl;
+
+                } else {
+                	
+                	/**
+                 * Local MySQL instance to use during development.
+                 */
+                 Class.forName("com.mysql.jdbc.GoogleDriver");
+                 url = googleUrl;   
+                 }
+            	
+                /**
+                 * Dann erst kann uns der DriverManager eine Verbindung mit den
+                 * oben in der Variable url angegebenen Verbindungsinformationen
+                 * aufbauen.
+                 * 
+                 * Diese Verbindung wird dann in der statischen Variable con
+                 * abgespeichert und fortan verwendet.
+                 */
+                con = (Connection) DriverManager.getConnection(url);
+                
+           /**
+            * con = (Connection) DriverManager.getConnection(googleUrl);
+            */
+            } catch (Exception e) {
+                con = null;
+                e.printStackTrace();
+            }
+        }
+
+        /**
+         * Zurückgegeben der Verbindung
+         */
+        return con;
+    }
+}

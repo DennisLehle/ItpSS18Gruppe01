f507f4eff78d6cfe352644ae192baf8da893af37
Dennis Lehle <dl043@hdm-stuttgart.de>
2018-04-29 13:30:30 +0200

    EntryPoint Klassen für Kontaktverwaltung angelegt

---
+++ src/de/hdm/itprojektss18/team01/sontact/client/Sontact.java
@@ -0,0 +1,30 @@
+package de.hdm.itprojektss18.team01.sontact.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Sontact implements EntryPoint {
+	
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+		
+	}
+}

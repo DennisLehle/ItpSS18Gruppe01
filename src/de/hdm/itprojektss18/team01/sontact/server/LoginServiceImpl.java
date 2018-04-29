6fb3e92016442ccd304b91a0e7c5a82bdfc86195
Dennis Lehle <dl043@hdm-stuttgart.de>
2018-04-29 13:32:24 +0200

    Serverseitige Klassen angelegt

---
+++ src/de/hdm/itprojektss18/team01/sontact/server/LoginServiceImpl.java
@@ -0,0 +1,12 @@
+package de.hdm.itprojektss18.team01.sontact.server;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+public class LoginServiceImpl extends RemoteServiceServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}

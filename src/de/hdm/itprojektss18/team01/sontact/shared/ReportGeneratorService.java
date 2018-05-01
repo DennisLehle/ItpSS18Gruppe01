2435bb2edc18ada0c1c86036bf230bc7641fc3fe
DennisLehle <dl043@hdm-stuttgart.de>
2018-05-01 10:23:27 +0200

    Report init() mehtode angelegt

--- src/de/hdm/itprojektss18/team01/sontact/shared/ReportGeneratorService.java
+++ src/de/hdm/itprojektss18/team01/sontact/shared/ReportGeneratorService.java
@@ -1,7 +1,10 @@
 package de.hdm.itprojektss18.team01.sontact.shared;
 
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.rpc.RemoteService;
 
 public interface ReportGeneratorService extends RemoteService{
+	
+	void init();
 
 }
--- src/de/hdm/itprojektss18/team01/sontact/shared/ReportGeneratorServiceAsync.java
+++ src/de/hdm/itprojektss18/team01/sontact/shared/ReportGeneratorServiceAsync.java
@@ -1,5 +1,9 @@
 package de.hdm.itprojektss18.team01.sontact.shared;
 
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
 public interface ReportGeneratorServiceAsync {
+
+	void init(AsyncCallback<Void> callback);
 
 }

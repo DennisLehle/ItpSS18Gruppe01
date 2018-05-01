64ea095acfc35a84bfe802a4aa3096992dc7c8e8
Ugur Bayrak <ub014@hdm-stuttgart.de>
2018-05-01 10:46:32 +0200

    Klasse CommonSettings erstellt

---
+++ src/de/hdm/itprojektss18/team01/sontact/shared/CommonSettings.java
@@ -0,0 +1,11 @@
+package de.hdm.itprojektss18.team01.sontact.shared;
+
+/**
+ * Zentrale Konstante etc., die sowohl Server- als auch Client-seitig benötigt
+ * werden. Superklasse von <code>ClientsideSettings</code> und
+ * <code>ServersideSettings</code>.
+ * 
+ */
+public class CommonSettings {
+
+}

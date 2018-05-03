8d9a85cd50b0d29b7ac4676b3dec7bd9e0ed3b92
YakupKanal <yk014@hdm-stuttgart.de>
2018-05-03 03:00:43 +0200

    DB: Finale Anpassung EigenschaftMapper - Bitte als Muster verwenden;

--- src/de/hdm/itprojektss18/team01/sontact/server/db/EigenschaftMapper.java
+++ src/de/hdm/itprojektss18/team01/sontact/server/db/EigenschaftMapper.java
@@ -1,57 +1,22 @@
 package de.hdm.itprojektss18.team01.sontact.server.db;
-import de.hdm.itprojektss18.team01.sontact.shared.bo.Eigenschaft;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Vector;
+import de.hdm.itprojektss18.team01.sontact.shared.bo.Eigenschaft;
 
 
-/**
- * Mapper-Klasse, die <code>Eigenschaft</code>-Objekte auf eine relationale
- * Datenbank abbildet. Hierzu wird eine Reihe von Methoden zur Verfügung
- * gestellt, mit deren Hilfe z.B. Objekte gesucht, erzeugt, modifiziert und
- * gelöscht werden können. Das Mapping ist bidirektional. D.h., Objekte können
- * in DB-Strukturen und DB-Strukturen in Objekte umgewandelt werden.
- * 
- * @author Thies
- */
 public class EigenschaftMapper {
 	
-	/**
-	 * Die Klasse EigenschaftMapper wird nur einmal instantiiert. Man spricht hierbei
-	 * von einem sogenannten <b>Singleton</b>.
-	 * <p>
-	 * Diese Variable ist durch den Bezeichner <code>static</code> nur einmal für
-	 * sämtliche eventuellen Instanzen dieser Klasse vorhanden. Sie speichert die
-	 * einzige Instanz dieser Klasse.
-	 * 
-	 * @see eigenschaftMapper()
-	 */
 	public static EigenschaftMapper eigenschaftMapper = null;
-	
-	/**
-	 * Geschützter Konstruktor - verhindert die Möglichkeit, mit "new" 
-	 * neue Instanzen dieser Klasse zu erzeugen
-	 */
-	protected EigenschaftMapper() {
+
+	protected EigenschaftMapper() {	
 		
 	}
 	
-	/**
-	 * Diese statische Methode kann aufgrufen werden durch
-	 * <code>EigenschaftMapper.eigenschaftMapper()</code>. Sie stellt die
-	 * Singleton-Eigenschaft sicher, indem Sie dafür sorgt, dass nur eine einzige
-	 * Instanz von <code>EigenschaftMapper</code> existiert.
-	 * <p>
-	 * 
-	 * <b>Fazit:</b> EigenschaftMapper sollte nicht mittels <code>new</code>
-	 * instantiiert werden, sondern stets durch Aufruf dieser statischen Methode.
-	 * 
-	 * @return DAS <code>EigenschaftMapper</code>-Objekt.
-	 * @see eigenschaftMapper
-	 */
+	
 	public static EigenschaftMapper eigenschaftMapper() {
 		if(eigenschaftMapper == null) {
 			eigenschaftMapper = new EigenschaftMapper();
@@ -89,6 +54,7 @@ public class EigenschaftMapper {
 		return e;
 	}
 	
+	
 	/**
 	 * Auslesen aller Eigenschaften welche zur Verfügung stehen. 
 	 * @return Eigenschaften
@@ -108,7 +74,7 @@ public class EigenschaftMapper {
 		// Vector anlegen
 		Vector <Eigenschaft> eigenschaften = new Vector <Eigenschaft>();
 		
-		// Ergebnistuppeln in Objekte umwandeln 
+		// Alle Ergebnistuppeln in Objekte umwandeln 
 		while (result.next()) {
 			Eigenschaft e = new Eigenschaft();
 			e.setId(result.getInt("id"));

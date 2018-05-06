83b78f335d6d34171c4f2ede9e4c8fcde04b42b9
Kevin Cataluna Batista <kc015@hdm-stuttgart.de>
2018-05-06 15:09:01 +0200

    server.db: Klasse AuspraegungMapper erstellt

--- src/de/hdm/itprojektss18/team01/sontact/server/db/AuspraegungMapper.java
+++ src/de/hdm/itprojektss18/team01/sontact/server/db/AuspraegungMapper.java
@@ -1,5 +1,215 @@
 package de.hdm.itprojektss18.team01.sontact.server.db;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Vector;
+import de.hdm.itprojektss18.team01.sontact.shared.bo.Auspraegung;
+import de.hdm.itprojektss18.team01.sontact.shared.bo.Eigenschaft;
+import de.hdm.itprojektss18.team01.sontact.shared.bo.Kontakt;
+
+
+/**
+ * Die Klasse <code>AuspraegungMapper</code> mappt auf der Datenbank alle
+ * Auspraegungen einer Eigenschaft.
+ * Für weitere Informationen:
+ * 
+ * @see NutzerMapper
+ * @author Kevin Batista
+ *
+ */
 public class AuspraegungMapper {
+
+	private static AuspraegungMapper auspraegungMapper = null;
+
+	/**
+	 * Geschützter Konstruktor
+	 */
+	protected AuspraegungMapper() {
+
+	}
+
+	/**
+	 * statische Methode zur Erzeugung von Instanzen, stellt die
+	 * Singleton-Eingeschaft sicher.
+	 */
+
+	public static AuspraegungMapper auspraegungMapper() {
+
+		if (auspraegungMapper == null) {
+			auspraegungMapper = new AuspraegungMapper();
+		}
+
+		return auspraegungMapper;
+
+	}
+	
+	/**
+	 * Einfügen eines Auspraegung-Objekts in die Datenbank.
+	 * @param a das zu speichernde Objekt
+	 * @return das bereits übergebene Objekt, 
+	 * jedoch mit ggf. korrigierter <code>id</code>.
+	 */	
+	public Auspraegung insert(Auspraegung a) {
+
+		// DBConnection herstellen
+		Connection con = DBConnection.connection();
+		
+		try {
+			// Leeres SQL Statement anlegen
+			Statement stmt = con.createStatement();
+			
+			ResultSet rs = stmt.executeQuery("SELECT MAX(id) AS maxid " + "FROM Auspraegung");
+			
+			if (rs.next()) {
+				a.setId(rs.getInt("maxid") + 1);
+				
+				// INSERT-Statement anlegen
+				PreparedStatement prestmt = con
+						.prepareStatement("INSERT INTO Auspraegung (id, wert, eigenschaftit, kontaktid, ownerid) "
+								+ "VALUES('" 
+								+ a.getId() + "', '" 
+								+ a.getWert() + "', '"
+								+ a.getEigenschaftId() + "', '" 
+								+ a.getKontaktId() + "', '" 
+								+ a.getOwnerid() + "')");
+		
+				// INSERT-Statement ausführen
+				prestmt.execute();
+				
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return a;
+
+	}
+	
+	/**
+	 * Aktualisierung eines Auspraegung-Objekts in der Datenbank.
+	 * @param a das Objekt, das in die DB geschrieben werden soll
+	 * @return das als Parameter übergebene Objekt
+	 */
+	public Auspraegung update(Auspraegung a) {
+		 Connection con = DBConnection.connection();
+		 
+		 try {
+			// UPDATE-Statement anlegen
+			 PreparedStatement prestmt = con.prepareStatement(
+						"UPDATE Auspraegung SET " 
+						+ "id = '" + a.getId() + "', "
+						+ "wert = '" + a.getWert() + "', "
+						+ "eigenschaftid = '" + a.getEigenschaftId() + "', "
+						+ "kontaktid = '" + a.getKontaktId() + "', "
+						+ "ownerid = '" + a.getOwnerid() + "')");
+			
+			 // INSERT-Statement ausführen
+			 prestmt.execute();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		 return a;
+	}
+	
+	/**
+	 * Löschen eines Auspraegung-Objekts aus der Datenbank.
+	 * @param a
+	 */
+	public void delete(Auspraegung a){
+		
+		Connection con = DBConnection.connection();
+		
+		try {
+			// DELETE-Statement anlegen
+			PreparedStatement prestmt = con.prepareStatement("DELETE FROM Auspraegung "
+					+ "WHERE id = " + a.getId());
+			
+			// DELETE-Statement ausführen
+			prestmt.execute();
+					
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	/**
+	 * Auspraegung anhand der eindeutig bestimmtbaren ID finden
+	 */
+	public Auspraegung findAuspraegungById(int id){
+		
+		Connection con = DBConnection.connection();
+		
+		try {
+			// SQL-Statement anlegen
+			PreparedStatement prestmt = con.prepareStatement(
+					"SELECT * FROM Auspraegung WHERE id = " + id);
+			
+		    // Statement ausführen
+		    ResultSet rs = prestmt.executeQuery();
+		    
+		    if (rs.next()) {
+		    	Auspraegung a = new Auspraegung();
+				a.setId(rs.getInt("id"));
+				a.setWert(rs.getString("wert"));
+				a.setEigenschaftId(rs.getInt("eigenschaftid"));
+				a.setKontaktId(rs.getInt("kontaktid"));
+				a.setOwnerid(rs.getInt("ownerid"));
+				
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+
+		
+	}
+	
+	
+	/**
+	 * Auspraegungen zu einem bestimmten Kontakt abrufen. 
+	 * @param Eigenschaft
+	 * @param Kontakt
+	 * @return Auspraegungsobjekte
+	 */
+	public Vector<Auspraegung> findAuspraegungByEigenschaft(Eigenschaft e, Kontakt k){
+		Connection con = DBConnection.connection();
+		Vector<Auspraegung> result = new Vector<Auspraegung>();
+		
+		try {
+			// SQL-Statement anlegen
+			PreparedStatement prestmt = con.prepareStatement(
+					"SELECT * FROM Auspraegung WHERE eigenschaftid = " + e.getId() + "AND kontaktid " + k.getId());
+			
+		    // Statement ausführen
+		    ResultSet rs = prestmt.executeQuery();
+		    
+		    while (rs.next()) {
+		    	Auspraegung a = new Auspraegung();
+				a.setId(rs.getInt("id"));
+				a.setWert(rs.getString("wert"));
+				a.setEigenschaftId(rs.getInt("eigenschaftid"));
+				a.setKontaktId(rs.getInt("kontaktid"));
+				a.setOwnerid(rs.getInt("ownerid"));
+				
+		        // Hinzufügen des neuen Objekts zum Ergebnisvektor
+		        result.addElement(a);
+				
+			}
+			
+		} catch (Exception exeception) {
+			exeception.printStackTrace();
+		}
+		
+		return result;
+		
+	}
 
 }

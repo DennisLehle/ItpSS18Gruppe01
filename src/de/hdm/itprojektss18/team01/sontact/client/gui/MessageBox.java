ef0ee165a950475c2a0c82465ae8f3c5e731dcca
Ugur Bayrak <ub014@hdm-stuttgart.de>
2018-05-16 19:48:08 +0200

    MessageBox hinzugefügt

---
+++ src/de/hdm/itprojektss18/team01/sontact/client/gui/MessageBox.java
@@ -0,0 +1,44 @@
+package de.hdm.itprojektss18.team01.sontact.client.gui;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HasAlignment;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class MessageBox {
+	
+	public static DialogBox alertWidget(final String header, final String content) {
+        final DialogBox box = new DialogBox();
+        final VerticalPanel panel = new VerticalPanel();
+        box.setText(header);
+        box.setGlassEnabled(true);
+        panel.add(new HTML(content));
+        final Button buttonClose = new Button("OK");
+        		
+        buttonClose.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				box.hide();
+
+			}
+		});
+        
+        
+        final Label emptyLabel = new Label("");
+        emptyLabel.setSize("auto","25px");
+        panel.add(emptyLabel);
+        panel.add(emptyLabel);
+        buttonClose.setWidth("90px");
+        panel.add(buttonClose);
+        panel.setCellHorizontalAlignment(buttonClose, HasAlignment.ALIGN_RIGHT);
+        box.add(panel);
+        box.center();
+        box.show();
+        return box;
+    }
+}

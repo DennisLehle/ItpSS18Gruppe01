8229c19c356d8ae51d3165dfa790485398dd66e4
Ugur Bayrak <ub014@hdm-stuttgart.de>
2018-05-20 18:03:10 +0200

    RegistrierungsFormular angepasst

--- src/de/hdm/itprojektss18/team01/sontact/client/gui/RegistrierungsFormular.java
+++ src/de/hdm/itprojektss18/team01/sontact/client/gui/RegistrierungsFormular.java
@@ -1,20 +1,30 @@
 package de.hdm.itprojektss18.team01.sontact.client.gui;
 
+import java.util.Vector;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 import de.hdm.itprojektss18.team01.sontact.client.ClientsideSettings;
 import de.hdm.itprojektss18.team01.sontact.shared.EditorServiceAsync;
+import de.hdm.itprojektss18.team01.sontact.shared.bo.Eigenschaft;
 import de.hdm.itprojektss18.team01.sontact.shared.bo.Nutzer;
 
 /**
  * Wird aufgerufen wenn Nutzer noch kein eigenes Profil im System besitzt.
  * 
+ * @author Ugur Bayrak, Kevin Batista, Dennis Lehle
  *
  */
+ 
 public class RegistrierungsFormular extends VerticalPanel {
 
 	private EditorServiceAsync ev = ClientsideSettings.getEditorVerwaltung();
@@ -27,22 +37,58 @@ public class RegistrierungsFormular extends VerticalPanel {
 	private TextBox nachnameTb = new TextBox();
 	private Button speichern = new Button("Speichern");
 
+	private Button auswahlEigenschaft = new Button("Auswahleigenschaft");
+	private Button eigeneEigenschaft = new Button("Eigene Eigenschaft hinzufuegen");
+
 	private HorizontalPanel hpvorname = new HorizontalPanel();
 	private HorizontalPanel hpnachnahme = new HorizontalPanel();
 
-	public RegistrierungsFormular(final Nutzer n) {
+	public RegistrierungsFormular(final Nutzer nutzer) {
 		this.add(titel);
 		hpvorname.add(vorname);
 		hpvorname.add(vornameTb);
 		hpnachnahme.add(nachname);
 		hpnachnahme.add(nachnameTb);
 		this.add(hpvorname);
-		this.add(hpvorname);
+		this.add(hpnachnahme);
 
-	}
+		this.add(speichern);
+		this.add(auswahlEigenschaft);
+		this.add(eigeneEigenschaft);
 
-	public void ladeFesteEigenschaften() {
-		//
+		auswahlEigenschaft.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				TextBox txtBox = new TextBox();
+				ListBox listBox = new ListBox();
+				ev.getEigenschaftAuswahl(new AsyncCallback<Vector<Eigenschaft>>() {
+
+					@Override
+					public void onFailure(Throwable error) {
+						error.getMessage().toString();
+
+					}
+
+					@Override
+					public void onSuccess(Vector<Eigenschaft> result) {
+						if (result != null) {
+							for (int i = 0; i < result.size(); i++) {
+								listBox.addItem(result.elementAt(i).getBezeichnung());
+							}
+						}
+
+					}
+
+				});
+
+				HorizontalPanel auswahlPanel = new HorizontalPanel();
+				auswahlPanel.add(listBox);
+				auswahlPanel.add(txtBox);
+				RootPanel.get("content").add(auswahlPanel);
+			}
+		});
+
 	}
 
 }

92a4140718f1b64b207e464264ccae4114972867
Ugur Bayrak <ub014@hdm-stuttgart.de>
2018-05-24 17:32:26 +0200

    KontaktlisteCell/ Form angelegt / Navigation TreeView impl folgt

---
+++ src/de/hdm/itprojektss18/team01/sontact/client/gui/KontaktlisteCell.java
@@ -0,0 +1,31 @@
+package de.hdm.itprojektss18.team01.sontact.client.gui;
+
+import com.google.gwt.cell.client.AbstractCell;
+import com.google.gwt.cell.client.Cell.Context;
+import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
+
+import de.hdm.itprojektss18.team01.sontact.shared.bo.Kontakt;
+import de.hdm.itprojektss18.team01.sontact.shared.bo.Kontaktliste;
+
+/**
+ * Stellt die Zelle einer Kontaktliste dar.
+ * WIrd für das TreeViewModel benötigt.
+ * 
+ * @author dennislehle
+ *
+ */
+public class KontaktlisteCell extends AbstractCell<Kontaktliste> {
+
+
+	@Override
+	public void render(Context context, Kontaktliste kl, SafeHtmlBuilder sb) {
+		if(kl == null) {
+			return;
+		}
+
+	  sb.appendHtmlConstant("<div>");
+      sb.appendEscaped(kl.getTitel());
+      sb.appendHtmlConstant("</div>");
+
+	}
+}

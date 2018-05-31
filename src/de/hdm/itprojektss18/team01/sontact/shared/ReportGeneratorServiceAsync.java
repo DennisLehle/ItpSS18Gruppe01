70834fed2a4e78c960d8eda3e9dbc977c91ea8fa
MieschaGotthilfAfshar <mg153@hdm-stuttgart.de>
2018-05-31 14:46:32 +0200

    Erster Entwurf des Report für die Ausgabe aller Kontakte eines Nutzers

--- src/de/hdm/itprojektss18/team01/sontact/shared/ReportGeneratorServiceAsync.java
+++
@@ -1,9 +0,0 @@
-package de.hdm.itprojektss18.team01.sontact.shared;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface ReportGeneratorServiceAsync {
-
-	void init(AsyncCallback<Void> callback);
-
-}

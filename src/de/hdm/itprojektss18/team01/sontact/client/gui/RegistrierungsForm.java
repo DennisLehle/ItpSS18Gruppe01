14c83b73cc816804842e322f608e01b77787f391
Kevin Cataluna Batista <kc015@hdm-stuttgart.de>
2018-06-02 14:27:41 +0200

    Erweiterung um weitere Auswahleigenschaften zu generieren

--- src/de/hdm/itprojektss18/team01/sontact/client/gui/RegistrierungsForm.java
+++ src/de/hdm/itprojektss18/team01/sontact/client/gui/RegistrierungsForm.java
@@ -10,16 +10,15 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.FlexTable;
-import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.ScrollPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
-import com.google.gwt.view.client.ProvidesKey;
 
 import de.hdm.itprojektss18.team01.sontact.client.ClientsideSettings;
 import de.hdm.itprojektss18.team01.sontact.shared.EditorServiceAsync;
@@ -45,15 +44,12 @@ public class RegistrierungsForm extends VerticalPanel {
 
 	Kontakt k = new Kontakt();
 	Nutzer n = new Nutzer();
-	
 
 	FlexTable kontaktFlex = new FlexTable();
 
 	ScrollPanel sp = new ScrollPanel();
-	
 
 	public RegistrierungsForm(Nutzer nutzer) {
-		
 
 		// RootPanel leeren damit neuer Content geladen werden kann.
 		RootPanel.get("content").clear();
@@ -69,11 +65,9 @@ public class RegistrierungsForm extends VerticalPanel {
 		gmailTb.setText(nutzer.getEmailAddress());
 		gmailTb.setEnabled(false);
 
-		// Button addEigenschaftBtn = new Button();
-
+		Button addEigenschaftBtn = new Button();
 
 		// Button f�r den Abbruch der Erstellung.
-
 		Button quitBtn = new Button("Abbrechen");
 		quitBtn.addClickHandler(new ClickHandler() {
 
@@ -120,48 +114,86 @@ public class RegistrierungsForm extends VerticalPanel {
 
 		@Override
 		public void onClick(ClickEvent event) {
-			
+
 			for (int i = 0; i < kontaktFlex.getRowCount(); i++) {
-				
+
 				Eigenschaft e = new Eigenschaft();
 				Widget w = kontaktFlex.getWidget(i, 0);
-				if(w instanceof TextBox) {
+				if (w instanceof TextBox) {
 					if (!((TextBox) w).getValue().isEmpty()) {
 						e.setBezeichnung(((TextBox) w).getValue());
-					};
+					}
+					;
 				}
-				
+
 				Auspraegung a = new Auspraegung();
 				Widget v = kontaktFlex.getWidget(i, 1);
-				if(v instanceof TextBox) {
-					if (!((TextBox)v).getValue().isEmpty()) {
-						a.setWert(((TextBox)v).getValue());
-					};
-						
+				if (v instanceof TextBox) {
+					if (!((TextBox) v).getValue().isEmpty()) {
+						a.setWert(((TextBox) v).getValue());
+					}
+					;
+
 				}
-				
-				
-					ev.createAuspraegungForNewEigenschaft(e.getBezeichnung(), a.getWert(), k,
-							new AsyncCallback<Void>() {
 
-								@Override
-								public void onFailure(Throwable caught) {
-									// TODO Auto-generated method stub
+				ev.createAuspraegungForNewEigenschaft(e.getBezeichnung(), a.getWert(), k, new AsyncCallback<Void>() {
 
-								}
+					@Override
+					public void onFailure(Throwable caught) {
+						// TODO Auto-generated method stub
 
-								@Override
-								public void onSuccess(Void result) {
-									Window.alert("passt");
+					}
 
-								}
-							});
+					@Override
+					public void onSuccess(Void result) {
+						Window.alert("passt");
 
-			
+					}
+				});
 
-			
 			}
 
+		}
+
+	}
+
+	/**
+	 * ClickHandler zum Generieren von weiteren Auswahleigenschaften.
+	 */
+
+	private class auswahleigenschaftClickHandler implements ClickHandler {
+
+		@Override
+		public void onClick(ClickEvent event) {
+
+			ListBox eigenschaftBox = new ListBox();
+			TextBox wertBox = new TextBox();
+
+			ev.getEigenschaftAuswahl(new AsyncCallback<Vector<Eigenschaft>>() {
+
+				@Override
+				public void onFailure(Throwable caught) {
+					caught.getMessage();
+
+				}
+
+				@Override
+				public void onSuccess(Vector<Eigenschaft> result) {
+					if (result != null) {
+
+						for (int i = 0; i < result.size(); i++) {
+							eigenschaftBox.addItem(result.elementAt(i).getBezeichnung());
+
+						}
+
+					}
+
+				}
+			});
+
+			int count = kontaktFlex.getRowCount();
+			kontaktFlex.setWidget(count + 1, 0, eigenschaftBox);
+			kontaktFlex.setWidget(count + 1, 1, wertBox);
 
 		}
 
@@ -175,14 +207,14 @@ public class RegistrierungsForm extends VerticalPanel {
 		@Override
 		public void onClick(ClickEvent event) {
 			final DialogBox db = new DialogBox();
-			Label bezeichnung = new Label("Bezeichnung:");
-			Label wer = new Label("Wert:");
+			Label bezlb = new Label("Bezeichnung:");
+			Label wertlbl = new Label("Wert:");
 			Button ok = new Button("OK");
 			TextBox bez = new TextBox();
 			TextBox wert = new TextBox();
 			FlexTable ft = new FlexTable();
-			ft.setWidget(0, 0, bezeichnung);
-			ft.setWidget(0, 1, wer);
+			ft.setWidget(0, 0, bezlb);
+			ft.setWidget(0, 1, wertlbl);
 			ft.setWidget(1, 0, bez);
 			ft.setWidget(1, 1, wert);
 			ft.setWidget(2, 2, ok);
@@ -192,7 +224,7 @@ public class RegistrierungsForm extends VerticalPanel {
 			db.setAnimationEnabled(true);
 			db.setAutoHideEnabled(true);
 			db.show();
-			
+
 			ok.addClickHandler(new ClickHandler() {
 
 				@Override
@@ -203,18 +235,17 @@ public class RegistrierungsForm extends VerticalPanel {
 						db.hide();
 						Eigenschaft eig = new Eigenschaft();
 						Auspraegung aus = new Auspraegung();
-						
+
 						eig.setBezeichnung(bez.getValue());
 						aus.setWert(wert.getValue());
-						
+
 						kontaktFlex.setWidget(kontaktFlex.getRowCount(), 0, bez);
-						kontaktFlex.setWidget(kontaktFlex.getRowCount() -1, 1, wert);
-						
-				
+						kontaktFlex.setWidget(kontaktFlex.getRowCount() - 1, 1, wert);
+
 					}
 				}
 			});
-			
+
 			RootPanel.get("content").add(kontaktFlex);
 
 		}
@@ -222,7 +253,7 @@ public class RegistrierungsForm extends VerticalPanel {
 	}
 
 	/**
-	 * ClickHandler zum Speichern eines neu angelegten Kontakts
+	 * ClickHandler zum Erstellen eines neu angelegten Kontakts
 	 */
 	public class kontaktErstellenClickHandler implements ClickHandler {
 
@@ -231,7 +262,7 @@ public class RegistrierungsForm extends VerticalPanel {
 			// Cookies des Nutzers holen.
 			n.setId(Integer.valueOf(Cookies.getCookie("nutzerID")));
 			n.setEmailAddress(Cookies.getCookie("nutzerGMail"));
-			
+
 			if (vornameTxtBox.getText().isEmpty() && nachnameTxtBox.getText().isEmpty()) {
 				MessageBox.alertWidget("Benachrichtigung", "Bitte mindestens Vor- und Nachname angeben");
 			} else {
@@ -251,25 +282,66 @@ public class RegistrierungsForm extends VerticalPanel {
 								HorizontalPanel headerPanel = new HorizontalPanel();
 								headerPanel.add(new HTML("<h2>Kontakteigenschaften angeben</h2>"));
 
-								Button createEigenschaftBtn = new Button("Eigenschaft erstellen");
 								HorizontalPanel BtnPanel = new HorizontalPanel();
+
+								Button addEigenschaftBtn = new Button("Weitere Auswahleigenschaften");
+								Button createEigenschaftBtn = new Button("Eigenschaft erstellen");
+
+								addEigenschaftBtn.addClickHandler(new auswahleigenschaftClickHandler());
 								createEigenschaftBtn.addClickHandler(new eigenschaftClickHandler());
+
+								BtnPanel.add(addEigenschaftBtn);
 								BtnPanel.add(createEigenschaftBtn);
+
 								Button speichernBtn = new Button("speichern");
 								speichernBtn.addClickHandler(new EigenschaftenSpeichern());
 								BtnPanel.add(speichernBtn);
-								
+
+								ListBox eigenschaftBox = new ListBox();
+								eigenschaftBox.getElement().setPropertyString("placeholder", "Auswahl");
+								TextBox wertBox = new TextBox();
+
+								ev.getEigenschaftAuswahl(new AsyncCallback<Vector<Eigenschaft>>() {
+
+									@Override
+									public void onFailure(Throwable caught) {
+										caught.getMessage();
+
+									}
+
+									@Override
+									public void onSuccess(Vector<Eigenschaft> result) {
+										if (result != null) {
+
+											for (int i = 0; i < result.size(); i++) {
+												eigenschaftBox.addItem(result.elementAt(i).getBezeichnung());
+
+											}
+
+										}
+
+									}
+								});
+
+								kontaktFlex.setWidget(0, 0, eigenschaftBox);
+								kontaktFlex.setWidget(0, 1, wertBox);
+
+								kontaktFlex.setWidget(1, 0, eigenschaftBox);
+								kontaktFlex.setWidget(1, 1, wertBox);
+
+								kontaktFlex.setWidget(2, 0, eigenschaftBox);
+								kontaktFlex.setWidget(2, 1, wertBox);
+
+								sp.add(kontaktFlex);
+								RootPanel.get("content").add(sp);
 								RootPanel.get("content").add(headerPanel);
 								RootPanel.get("content").add(BtnPanel);
-
 
 							}
 
 						});
 
 			}
-
-			
 
 		}
 	}

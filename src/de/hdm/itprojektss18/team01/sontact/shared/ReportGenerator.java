ea72d43e67afd26a321fd580ad6fd9a91f84f38a
MieschaGotthilfAfshar <mg153@hdm-stuttgart.de>
2018-06-05 15:27:00 +0200

    Erste Anpassungen zum Report

--- src/de/hdm/itprojektss18/team01/sontact/shared/ReportGenerator.java
+++ src/de/hdm/itprojektss18/team01/sontact/shared/ReportGenerator.java
@@ -4,9 +4,10 @@ import java.util.Vector;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
+import de.hdm.itprojektss18.team01.sontact.shared.report.AlleGeteiltenKontakteReport;
 import de.hdm.itprojektss18.team01.sontact.shared.bo.*;
-import de.hdm.itprojektss18.team01.sontact.shared.report.AllKontakteByNutzerReport;
-import de.hdm.itprojektss18.team01.sontact.shared.report.AllKontakteReport;
+import de.hdm.itprojektss18.team01.sontact.shared.report.AlleKontakteNachEigenschaftenReport;
+import de.hdm.itprojektss18.team01.sontact.shared.report.AlleKontakteReport;
 
 
 
@@ -15,10 +16,14 @@ public interface ReportGenerator extends RemoteService{
 	
 	void init();
 	
-	public AllKontakteByNutzerReport createAllKontakteByReport() 
+	public AlleKontakteNachEigenschaftenReport createAuspraegungReport() 
 			throws IllegalArgumentException;
 	
-	public AllKontakteReport createAllKontakteReport() 
+	public AlleKontakteReport createAlleKontakteReport() 
 			throws IllegalArgumentException;
+	
+	public AlleGeteiltenKontakteReport createAlleGeteilteReport() 
+			throws IllegalArgumentException;
+
 	
 }
---
+++ src/de/hdm/itprojektss18/team01/sontact/shared/report/AlleGeteiltenKontakteReport.java
@@ -0,0 +1,15 @@
+package de.hdm.itprojektss18.team01.sontact.shared.report;
+
+
+/**
+/**
+ * Reportklasse die eine Abfrage aller geteilten Kontakte eines Nutzers ausführt. 
+ * @author Miescha
+ */
+
+public class AlleGeteiltenKontakteReport extends SimpleReport
+{
+	private static final long serialVersionUID = 1L;
+
+
+}

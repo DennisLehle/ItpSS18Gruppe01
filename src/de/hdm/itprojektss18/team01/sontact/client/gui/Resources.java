b9df8e922afcfce04f2671ce59e31162c40115f6
DennisLehle <dl043@hdm-stuttgart.de>
2018-06-21 22:24:41 +0200

    Resources Interface für den Teilungsstatus eingeführt

---
+++ src/de/hdm/itprojektss18/team01/sontact/client/gui/Resources.java
@@ -0,0 +1,17 @@
+package de.hdm.itprojektss18.team01.sontact.client.gui;
+
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ImageResource;
+
+/**
+ * Dieses Interface stellt eine Resourcequelle für das Sharesymbol der 
+ * geteilten Eigenschaften/Auspraegungen der <code>ShowEigenschaft</code>-Klasse dar.
+ * Für mehr Infomationen nach CellTable.Resources (GWT Javadoc) googlen.
+ * 
+ * @author Dennis Lehle
+ *
+ */
+public interface Resources extends ClientBundle {
+	  @Source("shareAus.png")
+	  ImageResource getImageResource();
+	}

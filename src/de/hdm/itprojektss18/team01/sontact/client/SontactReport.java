cd1c7b9dbed7c8207f4111b5e249dd7663773cd3
Ugur Bayrak <ub014@hdm-stuttgart.de>
2018-06-29 12:54:05 +0200

    Klassen aufgerauemt + JavaDoc

--- src/de/hdm/itprojektss18/team01/sontact/client/SontactReport.java
+++ src/de/hdm/itprojektss18/team01/sontact/client/SontactReport.java
@@ -12,7 +12,6 @@ import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
-import de.hdm.itprojektss18.team01.sontact.client.gui.MessageBox;
 import de.hdm.itprojektss18.team01.sontact.client.gui.NavigationReport;
 import de.hdm.itprojektss18.team01.sontact.shared.EditorServiceAsync;
 import de.hdm.itprojektss18.team01.sontact.shared.LoginServiceAsync;
@@ -21,38 +20,40 @@ import de.hdm.itprojektss18.team01.sontact.shared.bo.LoginInfo;
 import de.hdm.itprojektss18.team01.sontact.shared.bo.Nutzer;
 
 /**
- * Die Klasse EntryPoint defniert die Methode <code>onModuleLoad()</code>. Diese Klasse ist der
- * Anfang des ReportGenerators. Der ReportGenerator soll nur statische HTML
- * ausgaben tätigen.
+ * Die Klasse EntryPoint defniert die Methode <code>onModuleLoad()</code>,
+ * welche beim Starten des Reports aufgerufen wird. Diese wird implementiert,
+ * damit eine Klasse als Moduleingangspunkt fungieren kann. Der ReportGenerator
+ * soll nur statische HTML ausgaben taetigen.
  * 
  * @author Ugur Bayrak, Kevin Batista, Dennis Lehle
- * 
  */
+
 public class SontactReport implements EntryPoint {
 
-	LoginInfo loginInfo = new LoginInfo();
+	private LoginInfo loginInfo = new LoginInfo();
 	private VerticalPanel loginPanel = new VerticalPanel();
-	private Label loginLabel = new Label("Bitte Melden Sie sich mit Ihren Google Account, um einen Zugriff auf den ReportGenerator zu bekommen.");
+	private Label loginLabel = new Label(
+			"Bitte Melden Sie sich mit Ihren Google Account, um einen Zugriff auf den ReportGenerator zu bekommen.");
+	private Anchor signOutLink = new Anchor("Logout");
+	private Anchor signInLink = new Anchor("Mit Google anmelden");
 	HTML loginHTML = new HTML("<h7></h7>");
 	HTML sontactHTML = new HTML("<h9>SONTACT-REPORT</h9>");
-	public Anchor signOutLink = new Anchor("Logout");
-	private Anchor signInLink = new Anchor("Mit Google anmelden");
 
-	
 	ClientsideSettings clientSettings = new ClientsideSettings();
-
 	LoginServiceAsync loginService = ClientsideSettings.getLoginService();
 	EditorServiceAsync editorVerwaltung = ClientsideSettings.getEditorVerwaltung();
 
 	/**
 	 * ReportService und EditorService werden auf null gesetzt. Diese werden neu
-	 * geladen, dies Dient zur Sicherheit.
+	 * geladen, dieser Schritt dient zur Sicherheit.
 	 */
 	ReportGeneratorAsync reportGeneratorService = null;
 	EditorServiceAsync editorService = null;
 
 	/**
-	 * Das ist die EntryPoint Methode <code>onModuleLoad()</code>	 
+	 * Das ist die EntryPoint Methode <code>onModuleLoad()</code>. Sie ist die
+	 * Einstiegspunktmethode, die automatisch aufgerufen wird, indem ein Modul
+	 * geladen wird, das eine implementierende Klasse als Einstiegspunkt deklariert.
 	 */
 	public void onModuleLoad() {
 
@@ -61,20 +62,22 @@ public class SontactReport implements EntryPoint {
 			@Override
 			public void onFailure(Throwable error) {
 				Window.alert("Fehler Login: " + error.toString());
-
 			}
 
 			@Override
 			public void onSuccess(LoginInfo result) {
 				loginInfo = result;
+
 				if (loginInfo.isLoggedIn() == true) {
+
+					// Nutzer wird erstellt
 					Nutzer n = new Nutzer();
 					n.setId(Integer.valueOf(Cookies.getCookie("nutzerID")));
 					n.setEmailAddress(Cookies.getCookie("nutzerGMail"));
 
 					// Laden der ReportSeite.
 					loadReport(n);
-					
+
 				} else {
 					loadLogin();
 				}
@@ -82,42 +85,49 @@ public class SontactReport implements EntryPoint {
 		});
 	}
 
-	private void loadLogin() {	
-		
+	/**
+	 * Die Methode loadLogin ladet den Login fuer den Report.
+	 */
+	private void loadLogin() {
 		signInLink.setHref(loginInfo.getLoginUrl());
 		loginPanel.add(loginHTML);
 		loginPanel.add(sontactHTML);
 		loginPanel.add(loginLabel);
 		loginPanel.add(signInLink);
 		loginPanel.addStyleName("login");
-		
+
+		// Footer wird geelert fuer den LoginContainer
 		RootPanel.get("footerR").clear();
+		// Hinzufuegen des LoginPanels in den <div> Container "content"
 		RootPanel.get("contentR").add(loginPanel);
 	}
 
-
 	private void loadReport(final Nutzer n) {
-		//MessageBox.alertWidget("Willkommen", "Hier können Sie ihren gewünschten Report generieren. ");
+		// MessageBox.alertWidget("Willkommen", "Hier können Sie ihren gewuenschten Report generieren.");
 		RootPanel.get("navigatorR").add(new NavigationReport(n));
-		
-		HTML willkommen = new HTML("<div align=\"center\"> <h8> <b> &nbsp; &nbsp; Willkommen beim Report Generator &nbsp; &nbsp; </b></h8> </div>");
-		
+		HTML willkommen = new HTML(
+				"<div align=\"center\"> <h8> <b> &nbsp; &nbsp; Willkommen beim Report Generator &nbsp; &nbsp; </b></h8> </div>");
+
 		RootPanel.get("contentR").add(willkommen);
-		RootPanel.get("contentR").add(new HTML("<div align=\"center\"> <image src='/images/ReportStart.png' width='350px' height='450px' align='center' /></div>"));
-		
+		RootPanel.get("contentR").add(new HTML(
+				"<div align=\"center\"> <image src='/images/ReportStart.png' width='350px' height='450px' align='center' /></div>"));
+
+		/**
+		 * Hinzufuegen der SontactReport.html und Sontact.html in den Footer.
+		 */
 		HorizontalPanel footer = new HorizontalPanel();
 		Anchor startseite = new Anchor("Startseite", "SontactReport.html");
-		HTML copyrightText1 = new HTML(" | ");
+		HTML Text1 = new HTML(" | ");
 		Anchor editorLink = new Anchor("Back 2 Sontact", "Sontact.html");
-		HTML copyrightText2 = new HTML(" | 2018 Sontact | ");
-		//Anchor impressumLink = new Anchor("Impressum");
-		
+		HTML Text2 = new HTML(" | 2018 Sontact | ");
+		// Anchor impressumLink = new Anchor("Impressum");
+
 		footer.add(startseite);
-		footer.add(copyrightText1);
+		footer.add(Text1);
 		footer.add(editorLink);
-		footer.add(copyrightText2);
+		footer.add(Text2);
 
+		// Das footerPanel wird dem <div> footer hinzugefuegt
 		RootPanel.get("footerR").add(footer);
-
 	}
 }
